fda976251a9e1e367b102b99690948b2ccdded75
Levon Aarakelyan <fizmatstepanakert@gmail.com>
2020-04-21 11:18:49 +0400

    Review was done (second)

--- db/migrate/20200415113112_give_default_value_to_todos_count.rb
+++
@@ -1,5 +0,0 @@
-class GiveDefaultValueToTodosCount < ActiveRecord::Migration[6.0]
-  def change
-    change_column_default :users, :todos_number, 0
-  end
-end
--- db/migrate/20200420061430_delete_todos_number.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTodosNumber < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :todos_number
-  end
-end
